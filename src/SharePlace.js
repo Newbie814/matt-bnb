4570c9a3c92c711b34e16f67a81fcb4fcd994b21
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-04-16 23:47:04 -0400

    added class for get user location

---
+++ src/SharePlace.js
@@ -0,0 +1,32 @@
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector('form');
+    const locateUserBtn = document.getElementById('locate-btn');
+
+    locateUserBtn.addEventListener('click', this.locateUserHandler);
+    addressForm.addEventListener('submit', this.findAddressHandler);
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert('Location is not available. Please enter an address.');
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        console.log(coordinates);
+      },
+      (error) => {
+        alert('Could not locate you. Please enter an address.');
+      }
+    );
+  }
+
+  findAddressHandler() {}
+}
+
+new PlaceFinder();
